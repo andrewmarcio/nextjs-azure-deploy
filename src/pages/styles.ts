a3ed888867ac8cacfbd813e42eaf4b2060a56a69
andrewmarcio <andrewmarcio21@gmail.com>
2024-06-10 20:30:56 -0300

    feat: add material ui to app

---
+++ src/pages/styles.ts
@@ -0,0 +1,10 @@
+import { Box, styled } from "@mui/material";
+
+const Container = styled(Box)(() => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+}))
+
+export { Container }
